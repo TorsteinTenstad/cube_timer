9141af91f78d2d5f9041e711ad60cbcb2afb020f
Torstein Tenstad <99toten@gmail.com>
2020-06-14 14:29:36 +0200

    Backup times

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 measures_of_interest = {'Single': [1, 0, 'C2', '\t\t'],
                         'Average of 5': [5, 1, 'C1', '\t'],
                         'Average of 12': [12, 1, 'C0', '\t'],
+                        'Average of 50': [50, 5, 'C4', '\t'],
                         'Average of 100': [100, 10, 'C3', '\t']}
